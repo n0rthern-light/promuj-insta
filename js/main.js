3ec9f12486e0e5d455720ceaa79ed683d9be454f
xapak <ske1337@gmail.com>
2019-02-10 01:00:33 +0100

    XD

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
+function getRandomArbitrary(min, max) {
+  return parseInt(Math.random() * (max - min) + min);
+}
+
 $(document).ready(function(){
   $('#start_btn').click(function(){
     $('#heroimage').fadeOut();
   });
+
+  $('#heroimage_content').fadeIn();
+
+  var random_start_likes = getRandomArbitrary(0, 2000);
+  var random_start_followers = getRandomArbitrary(0, 500);
+
+  $('#likes').html(random_start_likes);
+  $('#followers').html(random_start_followers);
+
+  $('#likes').delay(667).queue(function() {
+
+    $(this).animateNumber({ number: getRandomArbitrary(50000, 200000) });
+
+    $(this).dequeue();
+
+ });
+
+ $('#followers').delay(667).queue(function() {
+
+  $(this).animateNumber({ number: getRandomArbitrary(2000, 70000) });
+
+  $(this).dequeue();
+  });
+
+
 });
